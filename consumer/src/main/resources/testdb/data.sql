8217da6d75f37a83b0293dfc884f892dc21109bc
subhasubra <subramaniam.subhashini@gmail.com>
2020-12-03 21:07:48 +0530

    added initial data load into H2 DB

---
+++ consumer/src/main/resources/testdb/data.sql
@@ -0,0 +1,4 @@
+insert into customer values (1, 'john.doe@gmail.com', 'John Doe');
+insert into customer values (2, 'jane.doe@gmail.com', 'Jane Doe');
+insert into product  values (201, 'Product201', 201.00, 50);
+insert into product  values (202, 'Product202', 202.00, 50);
